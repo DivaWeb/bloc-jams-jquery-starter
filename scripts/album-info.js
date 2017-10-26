babfcb923db862fbfbd5789e4e10ca2fbe717978
Lanitta <lanittad@hotmail.com>
2017-10-26 12:46:27 -0700

    checkpoint11

---
+++ scripts/album-info.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  //title
+   $('#album-title').text(album.title);
+   //album art
+   $('img#ablum-cover-art').attr('src', album.albumArtUrl);
+   //artist
+   $('.artist').text(album.artist);
+   //release info
+   $('#release-info').text(album.releaseInfo);
+});
