0e2a0e089ce5ffe6848a82087534b0507e0507e5
Lanitta <lanittad@hotmail.com>
2017-10-31 21:17:11 -0700

    checkpoint assignment 13

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
      const previousSongIndex = currentSongIndex - 1;
-     if(previousSongIndex >= album.songs.length) {return;}
+     if(currentSongIndex < 1 ) {return;}
 
      const previousSong = album.songs[previousSongIndex];
      player.playPause(previousSong);
